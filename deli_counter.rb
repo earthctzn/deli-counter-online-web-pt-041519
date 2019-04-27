0a3a9211e7dd23179b65e6bd0bacccdbe92b1ea6
Caleb Aviles <earthctzn1@gmail.com>
2019-04-27 21:04:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,9 @@ def take_a_number(new, name)
   new.push name
   puts "Welcome, #{name}. You are number #{new.index(name) + 1} in line."
 end
+
+def now_serving(custy)
+  if custy.empty? == true 
+    puts "There is nobody waiting to be served!"
+  end
+  puts
