1ab170a0a5efdfdbd969a61d07f3d4cf46d77b06
Caleb Aviles <earthctzn1@gmail.com>
2019-04-27 20:55:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(array)
+phrase = "The line is currently:"
+  array.each.with_index {|name, index| phrase << " #{index + 1}. #{name}"}
+  if array == [] 
+     phrase = "The line is currently empty."
+  end
+    puts phrase
+end
+
+def take_a_number(new, name)
+end
